646719d41a83275d9f695a9d30464dcd00b6b093
heidi-ann <hh360@cam.ac.uk>
2015-03-24 17:20:08 +0000

    changing key from domain_name to Packet.answer

--- lib/cache.ml
+++ lib/cache.ml
@@ -1,3 +1,27 @@
+(*
+ * Copyright (c) 2015 Heidi Howard <hh360@cam.ac.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+module Store = struct
+  include Hashtbl
+
+  let find t k = 
+    try Some (Hashtbl.find t k) with | Not_found -> None
+
+end
+
 open Name
 
 type time = int32
@@ -5,7 +29,7 @@ type time = int32
 type t = { 
   max_size: int;
   mutable curr_size: int; 
-  data: (domain_name, time * Packet.rr list) Hashtbl.t;
+  data: (Packet.question, time * Packet.rr list) Store.t;
   }
 
 let min_ttl = Int32.of_int 0
@@ -14,7 +38,7 @@ let max_ttl = Int32.of_int 2147483647 (* max 1 week, same as dnscache *)
 let create n = {
   max_size = n;
   curr_size = 0;
-  data = Hashtbl.create (n/5);
+  data = Store.create (n/5);
   }
 
 let map_filter ~f xs = 
@@ -31,7 +55,9 @@ let lookup t time query =
   match query.questions with
     | [] -> (* QDCOUNT=0 *) None
     | (qu::_) -> (* assume QDCOUNT=1, ignore extra questions *)
-  let (init,rrs) = try Hashtbl.find t.data qu.q_name with | Not_found -> (Int32.zero,[]) in
+  match (Store.find t.data qu) with
+    | None -> None
+    | Some (init,rrs) ->
   let check_ttl rr =
     let diff = Int32.sub time init in
     let open Packet in
@@ -60,51 +86,54 @@ let rec add_pkt t time packet =
   match packet.questions with
     | [] -> (* QDCOUNT=0 *) ()
     | (qu::_) -> (* assume QDCOUNT=1, ignore extra questions *)
-  add t time qu.q_name packet.answers 
+  add t time qu packet.answers 
 
 
-and add t time name rrs =
+and add t time qu rrs =
   let rrs = List.filter filter_rr rrs in
   let if_no_space () = t.curr_size + List.length rrs > t.max_size in
   if if_no_space() then compress t time;
   if if_no_space() then compress_agressive t time (t.max_size - List.length rrs);
-  let length = try 
-    Hashtbl.find t.data name
-    |> fun (_,xs) -> List.length xs with | Not_found -> 0 in 
-  Hashtbl.replace t.data name (time,rrs);
+  let length = 
+    match Store.find t.data qu with
+      | Some (_,xs) -> List.length xs 
+      | None -> 0 in 
+  Store.replace t.data qu (time,rrs);
   t.curr_size <- (t.curr_size - length + (List.length rrs))
 
 
 and compress t time =
   let open Packet in
-  let take_action name (init,xs) = 
+  let take_action qu (init,xs) = 
     match List.filter (fun rr -> (Int32.sub time init) <= rr.ttl) xs with
-    | [] -> remove t name
-    | rrs -> replace t time name rrs in
-  Hashtbl.iter take_action t.data
+    | [] -> remove t qu
+    | rrs -> replace t time qu rrs in
+  Store.iter take_action t.data
 
-and remove t name = 
-  let length = try 
-    Hashtbl.find t.data name
-    |> fun (_,xs) -> List.length xs with | Not_found -> 0 in
-  Hashtbl.remove t.data name;
+and remove t qu = 
+  let length = 
+    match Store.find t.data qu with
+      | Some (_,xs) -> List.length xs 
+      | None -> 0 in 
+  Store.remove t.data qu;
   t.curr_size <- t.curr_size - length
 
-and replace t time name rrs = 
-  remove t name;
-  add t time name rrs
+and replace t time qu rrs = 
+  remove t qu;
+  add t time qu rrs
 
 and compress_agressive t _ new_size = 
   (* currently this naively removes RR's, 
   should be replaced with LRU or similar cache eviction policy *)
-  Hashtbl.iter (fun k _ -> if new_size<t.curr_size then remove t k) t.data
+  Store.iter (fun k _ -> if new_size<t.curr_size then remove t k) t.data
 
 
 let size t = t.curr_size
 
 let to_string t = 
+  let open Packet in
   let v_to_string = List.fold_left 
     (fun s rr -> Printf.sprintf "%s %s\n" s (Packet.rr_to_string rr)) "" in
-  Hashtbl.fold (fun k (t,v) s -> 
-    Printf.sprintf "%s%s %i\n%s" s (domain_name_to_string k) (Int32.to_int t) (v_to_string v))
+  Store.fold (fun k (t,v) s -> 
+    Printf.sprintf "%s%s %i\n%s" s (domain_name_to_string k.q_name) (Int32.to_int t) (v_to_string v))
   t.data (Printf.sprintf "Size [RRs] %i/%i\n" t.curr_size t.max_size)
--- lib/cache.mli
+++ lib/cache.mli
@@ -1,3 +1,19 @@
+(*
+ * Copyright (c) 2015 Heidi Howard <hh360@cam.ac.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
 open Name
 
 type time = int32
@@ -9,7 +25,7 @@ val create: int -> t
 val lookup: t -> time -> Packet.t -> Query.answer option
 
 val add_pkt: t -> time -> Packet.t -> unit
-val remove: t -> domain_name -> unit
+val remove: t -> Packet.question -> unit
 
 (** compress removes expired RR's, automatically called when there isn't enough space to add
 	resource records using the add function *)
